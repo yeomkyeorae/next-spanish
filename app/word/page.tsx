8c604191cb099842432926d74b64f3116c54200d
DESKTOP-Q17AD2N\duarufp06 <duarufp06@coretrust.com>
2023-08-22 16:16:52 +0900

    firebase test

--- app/word/page.tsx
+++ app/word/page.tsx
@@ -1,3 +1,25 @@
+'use client';
+import { useEffect } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { dbService } from '@/firebase/firebase';
+
 export default function Word() {
+  const testAddDoc = async () => {
+    try {
+      const docRef = await addDoc(collection(dbService, "word"), {
+        spanish: "Hablar",
+        korean: "말하다",
+        createdDate: new Date()
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  useEffect(() => {
+    testAddDoc();
+  }, []);
+
   return <div>Word!<button>추가</button></div>
 }
