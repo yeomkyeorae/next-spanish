08574240660aa900e06e100b398efca9d6576e20
DESKTOP-Q17AD2N\duarufp06 <duarufp06@coretrust.com>
2023-08-29 17:16:30 +0900

    some styles on main

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,29 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction } from 'react';
 
-export default function Input({ value, setValue, disabled }: { value: string, setValue?: Dispatch<SetStateAction<string>>, disabled?: boolean}) {
+export default function Input({
+  value,
+  setValue,
+  disabled,
+  title,
+}: {
+  value: string;
+  setValue?: Dispatch<SetStateAction<string>>;
+  disabled?: boolean;
+  title?: string;
+}) {
   return (
-    <input 
-      value={value} 
-      onChange={e => {
-        if(setValue) {
-          setValue(e.target.value)
-        }
-      }} 
-      disabled={disabled ?? false} 
-    />
+    <>
+      <h2 className='text-2xl'>{title ?? ''}</h2>
+      <input
+        value={value}
+        onChange={(e) => {
+          if (setValue) {
+            setValue(e.target.value);
+          }
+        }}
+        disabled={disabled ?? false}
+        className={'px-2 rounded-full ' + (disabled ? 'bg-gray-600 text-stone-100' : 'bg-zinc-200 text-stone-900')}
+      />
+    </>
   );
 }
--- components/papago.tsx
+++ components/papago.tsx
@@ -1,65 +1,83 @@
 'use client';
 
-import { useState } from "react";
-import Input from "./input";
-import axios from "axios";
+import { useState } from 'react';
+import Input from './input';
+import Button from './button';
+import axios from 'axios';
 
-const ToggleSourceContent: {[key: string]: string} = {
-  'ko': 'es',
-  'es': 'ko'
+const ToggleSourceContent: { [key: string]: string } = {
+  ko: 'es',
+  es: 'ko',
+};
+
+const DisplaySourceTarget: { [key: string]: any } = {
+  ko: '🇰🇷',
+  es: '🇪🇸',
 };
 
 export default function Papago() {
   const [toggleSource, setToggleSource] = useState('ko');
   const [sourceText, setSourceText] = useState('');
   const [targetText, setTargetText] = useState('');
-  
+
   const toggleHandler = () => {
     setToggleSource(ToggleSourceContent[toggleSource]);
-  }
+    setSourceText('');
+    setTargetText('');
+  };
 
   const translateHandler = async () => {
     const clientId = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
     const clientKey = process.env.NEXT_PUBLIC_NAVER_CLIENT_KEY;
 
-    if(clientId && clientKey) {
+    if (clientId && clientKey) {
       const headers = {
-        "Content-Type": "application/json",
-        "X-NCP-APIGW-API-KEY-ID": clientId,
-        "X-NCP-APIGW-API-KEY": clientKey,
+        'Content-Type': 'application/json',
+        'X-NCP-APIGW-API-KEY-ID': clientId,
+        'X-NCP-APIGW-API-KEY': clientKey,
       };
 
       try {
-        const response = await axios.post('/nmt/v1/translation', {
-          source: toggleSource,
-          target: ToggleSourceContent[toggleSource],
-          text: sourceText
-        }, {
-          headers
-        });
+        const response = await axios.post(
+          '/nmt/v1/translation',
+          {
+            source: toggleSource,
+            target: ToggleSourceContent[toggleSource],
+            text: sourceText,
+          },
+          {
+            headers,
+          },
+        );
 
-        if(response.data.message.result.translatedText) {
+        if (response.data.message.result.translatedText) {
           const result = response.data.message.result.translatedText;
-          if(result) {
+          if (result) {
             setTargetText(result);
           } else {
-
           }
-        }        
-
-      } catch(err) {
+        }
+      } catch (err) {
         console.log(err);
       }
     }
-  }
+  };
 
   return (
-    <div>
-      papago!
-      <button onClick={toggleHandler}>toggle {toggleSource} -- {ToggleSourceContent[toggleSource]}</button>
-      <Input value={sourceText} setValue={setSourceText} />
-      <Input value={targetText} disabled={true} />
-      <button onClick={translateHandler}>번역</button>
+    <div className='flex flex-col items-center gap-4'>
+      <span>
+        from {DisplaySourceTarget[toggleSource]} to {DisplaySourceTarget[ToggleSourceContent[toggleSource]]}
+      </span>
+      <div>
+        <Button text='바꾸기' onClickHandler={toggleHandler} />
+      </div>
+      <div className='flex flex-col w-3/4'>
+        <Input value={sourceText} setValue={setSourceText} title='입력' />
+        <Input value={targetText} disabled={true} title='출력' />
+      </div>
+      <div>
+        <Button text='번역' onClickHandler={translateHandler} />
+      </div>
     </div>
   );
 }
