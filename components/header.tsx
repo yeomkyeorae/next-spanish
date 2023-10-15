055c7255b8bc515a40f2fe5bc1ee1093901381a7
Kyeorae Yeom <duarufp06@gmail.com>
2023-10-15 17:36:48 +0900

    feat: login button to main page

--- components/header.tsx
+++ components/header.tsx
@@ -4,23 +4,24 @@ import { useAuthContext } from '@/context/authContext';
 import Link from 'next/link';
 
 export default function Header() {
-  const { user, login, logout } = useAuthContext();
+  const { user, logout } = useAuthContext();
 
   return (
     <header className='flex justify-between p-8'>
-      <Link href='/'>
-        <h1 className='text-2xl font-bold'>VamoSpain</h1>
-      </Link>
+      <h1 className='text-2xl font-bold'>VamoSpain</h1>
       <nav className='flex gap-4'>
         {user && (
           <>
             <Link href='/word'>Word</Link>
             <Link href='/sentence'>Sentence</Link>
             <Link href='/note'>Note</Link>
+            {
+              <button className='flex items-start' onClick={logout}>
+                로그아웃
+              </button>
+            }
           </>
         )}
-        {!user && <button onClick={login}>로그인</button>}
-        {user && <button onClick={logout}>로그아웃</button>}
       </nav>
     </header>
   );
