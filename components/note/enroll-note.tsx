f8eaf0a2c248e91d136fb045c435c46f5ae525a7
Kyeorae Yeom <duarufp06@gmail.com>
2023-10-26 23:15:07 +0900

    feat: define api for enroll & delete note, feat: change title, desc of meta

--- components/note/enroll-note.tsx
+++ components/note/enroll-note.tsx
@@ -1,10 +1,27 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import NoteMarkdown from './note-markdown';
+import { enrollNote } from '@/service/note';
+import { useAuthContext } from '@/context/authContext';
 
 export default function EnrollNote() {
   const [note, setNote] = useState('');
+  const { user } = useAuthContext();
+
+  const onEnrollHandler = useCallback(async () => {
+    const userId = user?.uid;
+
+    try {
+      if (userId) {
+        await enrollNote(userId, note);
+        alert('노트 등록에 성공했습니다!');
+      }
+    } catch (err) {
+      console.log(err);
+      alert('노트 등록에 실패했습니다!');
+    }
+  }, [note, user]);
 
   return (
     <section className='flex flex-col items-center'>
@@ -12,7 +29,9 @@ export default function EnrollNote() {
         <textarea className='w-72 h-72 border-2 border-black' value={note} onChange={(e) => setNote(e.target.value)} />
         <NoteMarkdown markdown={note} />
       </div>
-      <button className='w-32 h-8 bg-red-300 text-white rounded-md mt-6'>등록</button>
+      <button className='w-32 h-8 bg-red-300 text-white rounded-md mt-6' onClick={onEnrollHandler}>
+        등록
+      </button>
     </section>
   );
 }
