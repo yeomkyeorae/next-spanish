f1ba8e4b27746ab2c22db69a1efd94b1bec0d2dc
Kyeorae Yeom <duarufp06@gmail.com>
2023-12-11 23:42:49 +0900

    style: change height to min-height & broaden width

--- components/spanish/word.tsx
+++ components/spanish/word.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function Word({ spanish, korean, id, callback }: Props) {
   return (
-    <li className='flex items-center h-14 w-72 rounded-md border-2 mb-2 bg-red-500 text-white'>
+    <li className='flex items-center min-h-14 w-80 rounded-md border-2 mb-2 bg-red-500 text-white'>
       <div className='flex w-full'>
         <div className='flex flex-col w-11/12 p-3'>
           <span className='text-xl'>{spanish}</span>
