c05be9bba876e12799344fa67aa689a2319dc216
Kyeorae Yeom <duarufp06@gmail.com>
2024-07-24 13:37:09 +0900

    style: 단어, 문장 카드 너비가 일정하지 않음

--- components/spanish/Word.tsx
+++ components/spanish/Word.tsx
@@ -11,9 +11,9 @@ type Props = {
 
 export default function Word({ spanish, korean, id, modifyCallback, deleteCallback }: Props) {
   return (
-    <li className='flex items-center min-h-14 w-80 rounded-md border-2 mb-2 bg-red-500 text-white shadow-inner'>
-      <div className='flex w-full'>
-        <div className='flex flex-col w-11/12 p-3'>
+    <li className='flex items-center min-h-14 w-full rounded-md border-2 mb-2 bg-red-500 text-white shadow-inner'>
+      <div className='flex w-full justify-between'>
+        <div className='flex flex-col p-3'>
           <span className='text-xl'>{spanish}</span>
           <span className='text-sm'>{korean}</span>
         </div>
