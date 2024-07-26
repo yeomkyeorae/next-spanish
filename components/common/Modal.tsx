e4f4fec25c5340fa929b11745f9aa56f5afba517
Kyeorae Yeom <duarufp06@gmail.com>
2024-07-26 15:28:32 +0900

    feat: 단어 추가 정보 모달 input 추가

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -11,12 +11,15 @@ export default function Modal({ isOpen, onClose, children }: Props) {
 
   return (
     <div className='fixed inset-0 flex items-center justify-center z-50'>
-      <div className='fixed inset-0' onClick={onClose}></div>
-      <div className='bg-white rounded-lg shadow-lg p-6 z-10 relative w-3/4 lg:w-1/2'>
-        <button onClick={onClose} className='absolute top-0 right-0 mt-4 mr-4 text-gray-600'>
-          &times;
-        </button>
-        {children}
+      <div className='bg-white rounded-lg shadow-lg p-1 z-10 relative w-4/5 lg:w-1/2 flex justify-between'>
+        <div className='w-full flex flex-col'>
+          <div className='flex items-center flex-row-reverse'>
+            <button onClick={onClose} className='text-gray-600'>
+              &times;
+            </button>
+          </div>
+          {children}
+        </div>
       </div>
     </div>
   );
