a8c2ed3248ae38414c3fc0a9dee87a56d4828c32
Kyeorae Yeom <duarufp06@gmail.com>
2024-07-31 10:26:07 +0900

    refact: props 수정

--- components/additionalWordInfo/AdditionalWordInfoList.tsx
+++ components/additionalWordInfo/AdditionalWordInfoList.tsx
@@ -1,18 +1,24 @@
+import { WordInfo } from '@/types';
+
 interface Props {
-  data: { type: string; spanish: string }[];
+  data: WordInfo[];
 }
 
 export default function AdditionWordInfoList({ data }: Props) {
   return (
     <ul className='w-full flex flex-col items-center gap-1'>
-      {data.map((item, index) => {
-        return (
-          <li key={index} className='flex w-4/5 gap-1'>
-            <div className='border-2 border-black w-1/5 p-1'>{item.type}</div>
-            <div className='border-2 border-black w-4/5 p-1'>{item.spanish}</div>
-          </li>
-        );
-      })}
+      {data?.length > 0 ? (
+        data.map((item, index) => {
+          return (
+            <li key={index} className='flex w-4/5 gap-1'>
+              <div className='border-2 border-black w-1/5 p-1'>{item.explanation}</div>
+              <div className='border-2 border-black w-4/5 p-1'>{item.spanish}</div>
+            </li>
+          );
+        })
+      ) : (
+        <span>추가한 단어 정보가 없습니다</span>
+      )}
     </ul>
   );
 }
