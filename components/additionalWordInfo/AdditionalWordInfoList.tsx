91bd06fb13f15a31a418929e5f3e7e2f9bc6145f
Kyeorae Yeom <duarufp06@gmail.com>
2024-08-01 13:22:13 +0900

    feat: add buttons for modify, delete

--- components/additionalWordInfo/AdditionalWordInfoList.tsx
+++ components/additionalWordInfo/AdditionalWordInfoList.tsx
@@ -1,18 +1,29 @@
 import { WordInfo } from '@/types';
+import { FaPen } from 'react-icons/fa';
+import { IoMdRemoveCircle } from 'react-icons/io';
 
 interface Props {
   data: WordInfo[];
+  removeWordInfo: (wordId: string) => void;
 }
 
-export default function AdditionWordInfoList({ data }: Props) {
+export default function AdditionWordInfoList({ data, removeWordInfo }: Props) {
   return (
     <ul className='w-full flex flex-col items-center gap-1'>
       {data?.length > 0 ? (
         data.map((item, index) => {
           return (
             <li key={index} className='flex w-4/5 gap-1'>
-              <div className='border-2 border-black w-1/5 p-1'>{item.explanation}</div>
-              <div className='border-2 border-black w-4/5 p-1'>{item.spanish}</div>
+              <div className='border-2 border-black w-1/6 p-1'>{item.explanation}</div>
+              <div className='border-2 border-black w-3/6 p-1'>{item.spanish}</div>
+              <div className='border-2 border-black w-1/6 p-1 flex justify-center items-center gap-2'>
+                <span className='cursor-pointer'>
+                  <FaPen className='text-yellow-400 hover:text-carrot' />
+                </span>
+                <span onClick={() => removeWordInfo(item.id)} className='cursor-pointer'>
+                  <IoMdRemoveCircle className='text-yellow-400 hover:text-carrot' />
+                </span>
+              </div>
             </li>
           );
         })
