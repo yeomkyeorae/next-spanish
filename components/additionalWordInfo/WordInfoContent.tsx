e8d549928e5e416f250d142a514fa20f1b661344
Kyeorae Yeom <duarufp06@gmail.com>
2024-09-03 10:08:07 +0900

    feat: modify WordInfo

--- components/additionalWordInfo/WordInfoContent.tsx
+++ components/additionalWordInfo/WordInfoContent.tsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useState } from 'react';
 import Divider from '../common/Divider';
 import EnrollAdditionalWordInfo from './EnrollAdditionalWordInfo';
 import AdditionWordInfoList from './AdditionalWordInfoList';
-import { deleteWordInfo, getWordInfos } from '@/service/spanish';
+import { deleteWordInfo, getWordInfos, modifyWordInfo } from '@/service/spanish';
 import { useAuthContext } from '@/context/authContext';
 import { WordInfo } from '@/types';
 
@@ -18,6 +18,10 @@ interface Props {
 
 export default function WordInfoContent({ modalWordInfo }: Props) {
   const [wordInfos, setWordInfos] = useState<WordInfo[]>([]);
+  const [enrollType, setEnrollType] = useState<'추가' | '수정'>('추가');
+  const [spanish, setSpanish] = useState('');
+  const [explanation, setExplanation] = useState('');
+  const [modifyTargetId, setModifyTargetId] = useState('');
   const { user } = useAuthContext();
 
   const fetchWordInfos = useCallback(async () => {
@@ -63,6 +67,48 @@ export default function WordInfoContent({ modalWordInfo }: Props) {
     [fetchWordInfos],
   );
 
+  const changeWordInfo = useCallback(
+    async (id: string, spanish: string, explanation: string) => {
+      if (enrollType === '수정' && spanish && explanation) {
+        const ok = confirm('단어 추가 정보를 수정하시겠습니까?');
+
+        if (ok) {
+          try {
+            await modifyWordInfo(id, spanish, explanation);
+            fetchWordInfos();
+
+            alert('단어 추가 정보가 수정되었습니다!');
+
+            initInputs();
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      } else {
+        alert('입력 정보를 확인해 주세요!');
+      }
+    },
+    [enrollType, fetchWordInfos],
+  );
+
+  const initInputs = () => {
+    setEnrollType('추가');
+    setSpanish('');
+    setExplanation('');
+    setModifyTargetId('');
+  };
+
+  const changeEnrollModiType = useCallback(async (spanish: string, explanation: string, id: string) => {
+    setEnrollType('수정');
+    setSpanish(spanish);
+    setExplanation(explanation);
+    setModifyTargetId(id);
+  }, []);
+
+  const cancelModification = () => {
+    initInputs();
+  };
+
   useEffect(() => {
     fetchWordInfos();
   }, [modalWordInfo.wordId, fetchWordInfos]);
@@ -73,9 +119,20 @@ export default function WordInfoContent({ modalWordInfo }: Props) {
         {modalWordInfo.spanish} - {modalWordInfo.korean}
       </span>
       <Divider />
-      <EnrollAdditionalWordInfo wordId={modalWordInfo.wordId} fetchWordInfos={fetchWordInfos} />
+      <EnrollAdditionalWordInfo
+        wordId={modalWordInfo.wordId}
+        fetchWordInfos={fetchWordInfos}
+        enrollType={enrollType}
+        inputs={{ spanish, setSpanish, explanation, setExplanation, modifyTargetId }}
+        cancelModification={cancelModification}
+        changeWordInfo={changeWordInfo}
+      />
       <Divider />
-      <AdditionWordInfoList data={wordInfos} removeWordInfo={removeWordInfo} />
+      <AdditionWordInfoList
+        data={wordInfos}
+        removeWordInfo={removeWordInfo}
+        changeEnrollModiType={changeEnrollModiType}
+      />
     </section>
   );
 }
