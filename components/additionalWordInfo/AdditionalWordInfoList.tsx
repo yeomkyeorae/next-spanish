1ae7b8d70ae4cdbd1bbbdc56a7a99828be6f9c5d
Kyeorae Yeom <duarufp06@gmail.com>
2024-09-03 10:07:38 +0900

    feat: modify WordInfo

--- components/additionalWordInfo/AdditionalWordInfoList.tsx
+++ components/additionalWordInfo/AdditionalWordInfoList.tsx
@@ -5,22 +5,25 @@ import { IoMdRemoveCircle } from 'react-icons/io';
 interface Props {
   data: WordInfo[];
   removeWordInfo: (wordId: string) => void;
+  changeEnrollModiType: (spanish: string, explanation: string, id: string) => void;
 }
 
-export default function AdditionWordInfoList({ data, removeWordInfo }: Props) {
+export default function AdditionWordInfoList({ data, removeWordInfo, changeEnrollModiType }: Props) {
   return (
     <ul className='w-full flex flex-col items-center gap-1'>
       {data?.length > 0 ? (
         data.map((item, index) => {
+          const { spanish, explanation, id } = item;
+
           return (
             <li key={index} className='flex justify-center w-4/5 gap-1'>
-              <div className='border-2 border-black w-1/6 p-1 rounded-md'>{item.explanation}</div>
-              <div className='border-2 border-black w-4/6 p-1 rounded-md'>{item.spanish}</div>
+              <div className='border-2 border-black w-1/6 p-1 rounded-md'>{explanation}</div>
+              <div className='border-2 border-black w-4/6 p-1 rounded-md'>{spanish}</div>
               <div className='border-2 border-black w-1/6 p-1 rounded-md flex justify-center items-center gap-2'>
-                <span className='cursor-pointer'>
+                <span onClick={() => changeEnrollModiType(spanish, explanation, id)} className='cursor-pointer'>
                   <FaPen className='text-yellow-400 hover:text-carrot' />
                 </span>
-                <span onClick={() => removeWordInfo(item.id)} className='cursor-pointer'>
+                <span onClick={() => removeWordInfo(id)} className='cursor-pointer'>
                   <IoMdRemoveCircle className='text-yellow-400 hover:text-carrot' />
                 </span>
               </div>
