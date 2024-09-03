bda4ecfac9b0759384f38043d17838b0688c004b
Kyeorae Yeom <duarufp06@gmail.com>
2024-09-03 14:55:24 +0900

    style: set modal background opacity

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -10,7 +10,7 @@ export default function Modal({ isOpen, onClose, children }: Props) {
   if (!isOpen) return null;
 
   return (
-    <div className='fixed inset-0 flex items-center justify-center z-50'>
+    <div className='fixed inset-0 flex items-center justify-center z-50 bg-gray-500 bg-opacity-50'>
       <div className='bg-white rounded-lg shadow-lg p-1 z-10 relative w-4/5 lg:w-1/2 flex justify-between overflow-auto'>
         <div className='w-full flex flex-col'>
           <div className='flex items-center flex-row-reverse'>
