b91c5891540e9e172a9e06354a131059d211ef0e
Kyeorae Yeom <duarufp06@gmail.com>
2024-09-06 15:15:11 +0900

    feat: add title for note

--- components/note/MyNote.tsx
+++ components/note/MyNote.tsx
@@ -3,12 +3,13 @@ import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { useEffect } from 'react';
 
 type Props = {
+  title: string;
   content: string;
   requestBeforeNote: () => void;
   requestNextNote: () => void;
 };
 
-export default function MyNote({ content, requestBeforeNote, requestNextNote }: Props) {
+export default function MyNote({ title, content, requestBeforeNote, requestNextNote }: Props) {
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
       if (event.key === 'ArrowLeft') {
@@ -27,6 +28,9 @@ export default function MyNote({ content, requestBeforeNote, requestNextNote }: 
 
   return (
     <section className='flex flex-col items-center w-full h-full'>
+      <div className='w-4/5 md:w-6/12 mx-4 bg-white rounded-lg px-3 mb-2 font-bold text-lg'>
+        <h2>{title}</h2>
+      </div>
       <div className='flex items-center w-full md:w-7/12 h-full'>
         <div className='mx-2 text-white text-2xl cursor-pointer hover:text-highFever' onClick={requestBeforeNote}>
           <FaArrowLeft />
