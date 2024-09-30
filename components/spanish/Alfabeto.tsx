b28bc6535cd8e5dc56b05060e7d98c7fb20375cb
Kyeorae Yeom <duarufp06@gmail.com>
2024-09-30 09:55:59 +0900

    style: when selected change bg color

--- components/spanish/Alfabeto.tsx
+++ components/spanish/Alfabeto.tsx
@@ -1,12 +1,15 @@
 type Props = {
   word: string;
+  isCurrentWordSelected: boolean;
   onClickHandler?: () => void;
 };
 
-export default function Alfabeto({ word, onClickHandler }: Props) {
+export default function Alfabeto({ word, isCurrentWordSelected = false, onClickHandler }: Props) {
   return (
     <li
-      className='w-5 bg-red-400 rounded-md text-center text-xl text-white cursor-pointer hover:bg-red-300 transform transition-transform active:scale-110'
+      className={`w-5 rounded-md text-center text-xl text-white cursor-pointer hover:bg-red-300 transform transition-transform active:scale-110 ${
+        isCurrentWordSelected ? 'bg-highFever' : 'bg-red-400'
+      }`}
       onClick={onClickHandler}
     >
       {word}
