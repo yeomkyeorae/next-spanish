dba2e54c262749112484a24c324751e742fe756e
Kyeorae Yeom <duarufp06@gmail.com>
2024-10-07 13:57:28 +0900

    style: change disabled color as slate

--- components/common/icons/Star.tsx
+++ components/common/icons/Star.tsx
@@ -1,6 +1,6 @@
 import { FaStar } from 'react-icons/fa';
 
 export default function Star({ isChecked }: { isChecked: boolean }) {
-  const style = isChecked ? 'text-yellow-400' : '';
+  const style = isChecked ? 'text-yellow-400' : 'text-slate-300';
   return <FaStar className={style} />;
 }
